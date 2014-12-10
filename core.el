1a9013ec97c428faf4672c612d263f8bfe31919f
Victor M <samsaga2@gmail.com>
2014-12-10 21:18:05 +0100

    glsl mode

--- core.el
+++ core.el
@@ -1,8 +1,8 @@
 ;; hide menubar, toolbar, scrollbar and tooltips
+(menu-bar-mode -1)
 (when window-system
   (tooltip-mode -1)
   (tool-bar-mode -1)
-  (menu-bar-mode -1)
   (scroll-bar-mode -1))
 
 (when window-system
--- init.el
+++ init.el
@@ -124,6 +124,15 @@
 (nyan-start-animation)
 (nyan-mode)
 
+;; glsl mode
+(ensure-package-installed 'glsl-mode)
+(require 'glsl-mode)
+(autoload 'glsl-mode "glsl-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
+(add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
+
 ;; personal.el
 (if (file-exists-p "~/.emacs.d/personal.el")
     (load "~/.emacs.d/personal.el"))
